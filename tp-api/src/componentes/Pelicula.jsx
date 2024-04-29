ac5e4bf6938cab4a826fc390bff306e59268bc0e
lbagnus <lara.bagnus@gmail.com>
2024-04-29 15:09:42 -0300

    borre vars

--- tp-api/src/componentes/Pelicula.jsx
+++ tp-api/src/componentes/Pelicula.jsx
@@ -12,14 +12,6 @@ import reparto4 from '../imagenes/actores/jdeep-modified.png'
 import director1 from '../imagenes/actores/jdeep-modified.png'
 import director2 from '../imagenes/actores/jdeep-modified.png'
 
-
-
-
-var titulo = "El padrino";
-var puntaje = "4.7";
-var anio = "1972";
-var tipo = "Drama";
-var desc_padrino = "En el verano de 1945, se celebra la boda de Connie (Talia Shire) y Carlo Rizzi (Gianni Russo). Connie es la única hija de Don Vito Corleone (Marlon Brando), jefe de una de las familias que ejercen el mando de la Cosa Nostra en la ciudad de Nueva York. Don Vito además tiene otros tres hijos: su primogénito Sonny (James Caan), el endeble Fredo (John Cazale) y el más joven se todos, Michael (Al Pacino), un marine condecorado por su lucha en la Segunda Guerra Mundial que acaba de regresar a su hogar en Nueva York. Por designios de la fortuna, Michael terminará llevando la vida que no deseaba, tomando las riendas del negocio familiar, cambiando su moral y sus valores, para defender a toda costa a su familia.";
 var nombrereparto1 = "Sandra Bullock"
 var nombrereparto2 = "Sandra Bullock"
 var nombrereparto3 = "Sandra Bullock"
